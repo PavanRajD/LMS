19005340a7d032b0ca5e552162acd48fe2758398
Pavan Raj <Pavan.r@keka.com>
2021-01-19 10:29:29 +0530

    first commit

---
+++ SampleAPI/Controllers/ManageBooksController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SampleAPI.Models;
+
+namespace SampleAPI.Controllers
+{
+    [ApiController]
+    [Route("api/books/manage")]
+    public class ManageBooksController : ControllerBase
+    {
+        public ManageBooksController()
+        {
+        }
+
+        [Route("add")]
+        public bool AddBook([FromBody] book book)
+        {
+            return true;
+        }
+
+        [Route("{bookId}/update")]
+        public bool UpdateBook(int bookId, [FromBody] book book)
+        {
+            return true;
+        }
+
+        [Route("{bookId}/delete")]
+        public bool DeleteBook(int bookId)
+        {
+            return true;
+        }
+    }
+}
---
+++ SampleAPI/Controllers/ManageUsersController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SampleAPI.Controllers
+{
+    [ApiController]
+    [Route("api/library/users/{userId}")]
+    public class LibraryUserController : ControllerBase
+    {
+        public LibraryUserController()
+        {
+        }
+
+        [Route("borrow/{bookId}")]
+        public bool PostBorrowBook(int userId, int bookId)
+        {
+            return true;
+        }
+
+        [Route("return/{bookId}")]
+        public bool PutReturnBook(int userId, int bookId)
+        {
+            return true;
+        }
+
+        [Route("logs")]
+        public IEnumerable<string> GetUserLogs(int userId)
+        {
+            return new List<string>();
+        }
+    }
+}
